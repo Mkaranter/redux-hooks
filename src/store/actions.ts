80e295f493dea83947c4ee1be4aa513827aaf24c
Michal Karanter <michalkaranter@MBP-Michal.skygate.io>
2019-09-18 16:15:52 +0200

    update actions

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,7 +1,11 @@
 export const INCREMENT = 'INCREMENT';
-
 export const incrementDonuts = () => {
   return { type: INCREMENT };
+};
+
+export const DECREMENT = 'DECREMENT';
+export const decrementDonuts = () => {
+  return { type: DECREMENT };
 };
 
 export interface Action {
